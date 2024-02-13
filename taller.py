dfc9a40ebde80f7fa4a57a1afad47784b24e4c1d
shanmukhnagasai <156528509+shanmukhnagasai@users.noreply.github.com>
2024-02-13 10:16:44 +0530

    Add files via upload
    commit

---
+++ taller.py
@@ -0,0 +1,17 @@
+print("welcome to roller coster!")
+
+height=int(input("what is your height in cm?"))
+
+if height >=120:
+    print("you can ride the roller coaster!")
+    age= int(input("what is your age?"))
+if age < 12:
+    print("please pay$5.")
+
+    print("please pay $7.")
+else:
+     print("please pay $12.")
+else:
+
+ print("play the roller coaster")
+    
